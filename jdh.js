a88f96073e37063547bd055bf5caea50572c89d3
Thyago-limma <102415139+Thyago-limma@users.noreply.github.com>
2022-04-14 20:25:27 -0300

    Update jdh.js

--- jdh.js
+++ jdh.js
@@ -2,7 +2,10 @@ const prompt = require('prompt-sync')();
 let respostas = 0;
 
 console.clear
-console.log('Um Guerreiro que Havia Morrido em Batalha, Chamado Link, Precisaria Voltar do Inferno Para Concluir Sua Vingança Contra Aquele que o Matou. Mas Para Isso, Ele Precisa Realizar a Missão de Coletar 5 Jóias que São os Amuletos Para Ele Poder Sair do Inferno');
+console.log(`Um Guerreiro que Havia Morrido em Batalha, Chamado Link,
+Precisaria Voltar do Inferno Para Concluir Sua Vingança Contra Aquele que o Matou.
+Mas Para Isso, Ele Precisa Realizar a Missão de Coletar 5 Jóias que São os Amuletos
+Para Ele Poder Sair do Inferno`);
 console.log('Link Pega uma Espada Que Estava Ao Seu Lado e Parte pra Sua Missão.');
 
 console.log();
@@ -104,11 +107,16 @@ do {
   } else if (respostas === 4) {
       console.log('Link Conseguiu Escapar do Inferno, E Ter Sua Vingança,Mas Vai Ser Sempre Atormentado Pelo Espirito daquele que Ele Matou.');
   }else if (respostas === 3) {
-      console.log('Mesmo Lutando e Usando Todas as Suas Artimanhas Link Conseguiu Sair do Inferno,Mas Não Conseguiu Concluir Sua Vingança. Sendo Assim Condenado a Vagar Pela Terra Como um Espirito Vingativo.');
+      console.log(`Mesmo Lutando e Usando Todas as Suas Artimanhas Link Conseguiu Sair do Inferno,
+      Mas Não Conseguiu Concluir Sua Vingança.
+      Sendo Assim Condenado a Vagar Pela Terra Como um Espirito Vingativo.`);
   }else if (respostas === 2 || respostas ===1) {
-      console.log('Link fez de Tudo pra Sair e Concluir Sua Vingança,Mas Lutando Contra Todos os Obstáculos Você Percebeu que Não Valia a Pena se Vingar e Assim Buscou Redenção. Continua Morto,Mas Agora em Um Lugar Melhor');
+      console.log(`Link fez de Tudo pra Sair e Concluir Sua Vingança,
+      Mas Lutando Contra Todos os Obstáculos Você Percebeu que Não Valia a Pena se Vingar e Assim Buscou Redenção.
+      Continua Morto,Mas Agora em Um Lugar Melhor`);
   }else if (respostas === 0) {
-      console.log('Link Não Estava Preparado para Lutar e Conseguir as Jóias que lhe Tirassem de Onde Ele Estava. Assim, Tendo Que Viver num Mundo de Torturas Físicas e PSicologica, Por Toda Eternidade.')
+      console.log(`Link Não Estava Preparado para Lutar e Conseguir as Jóias que lhe Tirassem de Onde Ele Estava.
+      Assim, Tendo Que Viver num Mundo de Torturas Físicas e PSicologica, Por Toda Eternidade.`)
   }
 
 
